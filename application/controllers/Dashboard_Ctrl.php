f20fc0c3f356a0e4972aa592f7e754220f73f23c
Qasim-Alamgir <qaxim12@gmail.com>
2021-04-16 23:12:47 +0500

    Add component in Insurance and Family History

--- application/controllers/Dashboard_Ctrl.php
+++ application/controllers/Dashboard_Ctrl.php
@@ -12,38 +12,34 @@ class Dashboard_Ctrl extends MY_Controller {
 //////////////////////////------- For Dashboard/.php --------/////////////////////////////////
 
 public function dashboard(){
+	$post = get_request_body();
     $query = $query2 = "";
     $pid = $this->user_id;
     //Allergy
-		if ($_REQUEST['type'] == 'allergy') {
+		if ($post['type'] == 'allergy') {
 			$query = "select name from patient_allergies where endDate IS NULL and pid = ".$pid." limit 10";
 			//exit($query);  
-		} else if ($_REQUEST['type'] == 'medical') {
+		} else if ($post['type'] == 'medical') {
 			$query = "select *, IF(active = '1','Yes','No') as activeMed from medicalhx where active = 1 and pid=".$pid." limit 10";		
-		} else if ($_REQUEST['type'] == 'bp') {
+		} else if ($post['type'] == 'bp') {
 			$query = "select *,MonthName(date) from form_vitals where pid = ".$pid." order by date asc limit 10";
 			$query2 = "select *,MonthName(date) from vitals_readings where pid = ".$pid." and vital_type = 'bp' order by date asc limit 10";
-		} else if ($_REQUEST['type'] == 'bmi') {
+		} else if ($post['type'] == 'bmi') {
 			$query = "select *,MonthName(date) from form_vitals where pid = ".$pid." order by date asc limit 5";
 			$query2 = "select *,MonthName(date) from vitals_readings where pid = ".$pid." and vital_type = 'BMI' order by date asc limit 5";
-		} else if ($_REQUEST['type'] == 'cholestrol') {
+		} else if ($post['type'] == 'cholestrol') {
 			$query = "select * from patient_cholesterol_data a where a.pid = ".$pid." order by a.cholesterol_id desc limit 5";
 		}
 
-	
-		$result = $this->Dashboard_model->dynamic_query_execution($query);
-		$rows = array();
-		print_r($rows);
-		while ($r = $result)
-			$rows[] = $r;
-
+	$result = [];
+		$result1 = $this->Dashboard_model->dynamic_query_execution($query);
+		// array_merge($result, $result1);
 		if($query2 <> ""){
-			$result = $this->Dashboard_model->dynamic_query_execution($query2);
-			while ($r = $result)
-				$rows[] = $r;
+			$result2 = $this->Dashboard_model->dynamic_query_execution($query2);
+			$result1 = array_merge($result1, $result2);
 		}
 	
-		echo json_encode($rows);
+		echo json_encode($result1);
 
 }
 
