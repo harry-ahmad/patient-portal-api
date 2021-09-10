272d1225ce26d203cf70002cef3e8ab7255f1317
Qasim-Alamgir <qaxim12@gmail.com>
2021-09-10 16:07:06 +0500

    show inactive insurance record

--- application/models/Insurance_model.php
+++ application/models/Insurance_model.php
@@ -11,7 +11,7 @@ public function insurance_data_all($pid)
     $this->db->or_where('sort_order','2');
     $this->db->or_where('sort_order','3');
     $this->db->where('encounter_id',0);
-    $this->db->where('mystatus','1');
+    // $this->db->where('mystatus','1');
     $this->db->order_by("id", "asc");
     // $this->db->limit(1);
     $query = $this->db->get();
@@ -30,7 +30,7 @@ public function insurance_data_one($pid)
     $this->db->where('pid',$pid);
     $this->db->where('sort_order','1');
     $this->db->where('encounter_id',0);
-    $this->db->where('mystatus','1');
+    // $this->db->where('mystatus','1');
     $this->db->order_by("id", "asc");
     $this->db->limit(1);
     $query = $this->db->get();
@@ -49,7 +49,7 @@ public function insurance_data_two($pid)
     $this->db->where('pid',$pid);
     $this->db->where('sort_order','2');
     $this->db->where('encounter_id',0);
-    $this->db->where('mystatus','1');
+    // $this->db->where('mystatus','1');
     $this->db->order_by("id", "asc");
     $this->db->limit(1);
     $query = $this->db->get();
@@ -68,7 +68,7 @@ public function insurance_data_three($pid)
     $this->db->where('pid',$pid);
     $this->db->where('sort_order','3');
     $this->db->where('encounter_id',0);
-    $this->db->where('mystatus','1');
+    // $this->db->where('mystatus','1');
     $this->db->order_by("id", "asc");
     $this->db->limit(1);
     $query = $this->db->get();
